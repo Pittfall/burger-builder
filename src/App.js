91649d918ff71afe86e96a9019571233eafcaf21
arusu <arusu@sehcomp.ca>
2018-07-26 11:56:57 -0400

    Added a root layout component that will contain the root layout elements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        { /* This can be the root component but I kept App as the root component in case layout changes and I need
+        to change something at the root level. */}
+        <Layout></Layout>
       </div>
     );
   }
