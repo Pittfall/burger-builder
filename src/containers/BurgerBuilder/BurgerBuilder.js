582dadda601c68bea6f402fb6b8e1576c922662c
arusu <arusu@sehcomp.ca>
2018-07-27 08:03:33 -0400

    Added build controls so that ingredients can be added or removed from a burger

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,14 @@
 import React, { Component, Fragment } from 'react';
 
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
+
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 1
+}
 
 class BurgerBuilder extends Component {
     state = {
@@ -9,14 +17,51 @@ class BurgerBuilder extends Component {
             bacon: 0,
             cheese: 0,
             meat: 0
+        },
+        totalPrice: 4
+    }
+
+    addIngredientHandler = (type) => {
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type]++;
+
+        let newPrice = this.totalPrice;
+        newPrice += INGREDIENT_PRICES[type];
+
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+    }
+
+    removeIngredientHandler = (type) => {
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type]--;
+
+        if (updatedIngredients[type] < 0) {
+            return;
         }
+
+        let newPrice = this.totalPrice;
+        newPrice -= INGREDIENT_PRICES[type];
+
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
     }
 
     render () {
+        const disabledInfo = {
+            ...this.state.ingredients
+        };
+        for (let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0;
+        }
+
         return (
             <Fragment>
                 <Burger ingredients={this.state.ingredients} />
-                <div>Build Controls</div>
+                <BuildControls ingredientAdded={this.addIngredientHandler} 
+                ingredientRemoved={this.removeIngredientHandler} disabled={disabledInfo}/>
             </Fragment>
 
         );
