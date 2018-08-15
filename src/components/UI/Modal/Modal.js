dd72f285041d494a028a445eaab6c85642b14c3f
arusu <arusu@sehcomp.ca>
2018-08-15 11:48:46 -0400

    conditionally render the modal only if the show property is set to true

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -8,12 +8,15 @@ const modal = (props) => {
     const visibility = props.show ? classes.Show : classes.NoShow;
     modalClasses += ' ' + visibility;
 
+    // Another solution is to turn this into a class component and use the,
+    // shouldComponentUpdate method to check if the show property changed.
+    const modalContent = props.show ? props.children : null;
 
     return (
         <Fragment>
             <Backdrop show={props.show} clicked={props.modalClosed}/>
             <div className={modalClasses}>
-                {props.children}
+                {modalContent}
             </div>
         </Fragment>
     )
