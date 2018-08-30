da34a37fdc042fffd22c9bad672f9888d191c47d
arusu <arusu@sehcomp.ca>
2018-08-30 15:08:20 -0400

    Checkpoint in trying to style burger component in 2 different places

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends Component {
   render() {
@@ -8,7 +9,10 @@ class App extends Component {
       <div>
         { /* This can be the root component but I kept App as the root component in case layout changes and I need
         to change something at the root level. */}
-        <Layout><BurgerBuilder /></Layout>
+        <Layout>
+          <BurgerBuilder />
+          <Checkout />
+        </Layout>
       </div>
     );
   }
